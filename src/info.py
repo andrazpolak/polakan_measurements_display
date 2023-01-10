ec549207530a75a83245d4acdd72bc658646c43f
andrazpolak <andrazpolak@gmail.com>
2023-01-10 14:44:54 +0100

    Initial commit

---
+++ src/info.py
@@ -0,0 +1,11 @@
+import atpthings as atp
+import pandas as pd
+
+print("_______________")
+print("Script [start]:", __file__)
+
+print("atpthings [Version]:", atp.__version__)
+print("pandas [Version]:", pd.__version__)
+
+print("Script [end]:", __file__)
+print("_______________")
